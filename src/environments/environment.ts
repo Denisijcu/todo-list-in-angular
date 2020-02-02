94c61eb06d334f7b2129b5a9c5d4f8445f85861a
Denis Sanchez Leyva <denisijcu266@gmail.com>
2020-02-01 22:45:36 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyChPbeazTDL5ahSvezYWoXFWe6LS1VYMbQ',
-    authDomain: 'todo-list-b2efd.firebaseapp.com',
-    databaseURL: 'https://todo-list-b2efd.firebaseio.com',
-    projectId: 'todo-list-b2efd',
-    storageBucket: 'todo-list-b2efd.appspot.com',
-    messagingSenderId: '799544492504'
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: ''
   }
 };
 
